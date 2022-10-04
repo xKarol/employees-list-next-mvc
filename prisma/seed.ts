924d1a1dce709519abda2502314bc49d60a39531
xKarol <karolpiskorz8@gmail.com>
2022-10-04 16:34:06 +0200

    Add simple create employee form using react-hook-form

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,7 +4,6 @@ const prisma = new PrismaClient();
 const seed = async () => {
   console.log("Seeding...");
   prisma.user.create({ data: { email: "test@gmail.com" } });
-  console.log("Seeding Completed!");
 };
 
 seed();

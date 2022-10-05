fe089c3f961b6d01027d1881c8994e54b9b8a00a
xKarol <karolpiskorz8@gmail.com>
2022-10-05 16:04:36 +0200

    Add seeding database with fake data

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,19 +1,31 @@
-import { PrismaClient } from "@prisma/client";
+import { faker } from "@faker-js/faker";
+import { Employee, PrismaClient } from "@prisma/client";
+
 const prisma = new PrismaClient();
+const EMPLOYEES_CREATE_AMOUNT = 100;
 
 const seed = async () => {
-  console.log("Seeding...");
-  prisma.employee.create({
-    data: {
-      firstName: "firstName",
-      lastName: "lastName",
-      email: "email",
-      pesel: "pesel",
-      zipCode: "zipcode",
-      city: "city",
-      phone: "phone",
-    },
-  });
+  try {
+    console.log("\x1b[32m", "Seeding...", "\x1b[0m");
+    await prisma.employee.deleteMany({});
+    const employeesArray: Employee[] = new Array(EMPLOYEES_CREATE_AMOUNT).fill({});
+
+    const employeesList = employeesArray.map(() => ({
+      firstName: faker.name.firstName(),
+      lastName: faker.name.lastName(),
+      email: faker.internet.email(),
+      pesel: `${faker.datatype.number({ min: 10000000000, max: 90000000000 })}`,
+      zipCode: faker.address.zipCode(),
+      city: faker.address.cityName(),
+      phone: faker.phone.number(),
+    }));
+
+    await prisma.employee.createMany({
+      data: employeesList,
+    });
+  } catch (error) {
+    console.log("\x1b[31m", error, "\x1b[0m");
+  }
 };
 
 seed();

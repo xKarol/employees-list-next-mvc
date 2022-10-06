3dd2c3708c82d09ed9b82a6646cf5602769f35c2
xKarol <karolpiskorz8@gmail.com>
2022-10-06 20:05:30 +0200

    Minor employee-form changes

--- features/employee/containers/employee-form.tsx
+++ features/employee/containers/employee-form.tsx
@@ -20,14 +20,7 @@ const EmployeeFormContainer = () => {
   });
   const { mutate, isLoading, isError, error } = useMutation(createEmployee);
 
-  const onSubmit: SubmitHandler<EmployeeType> = (data) => {
-    try {
-      mutate(data);
-      console.log(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  const onSubmit: SubmitHandler<EmployeeType> = (data) => mutate(data);
 
   return (
     <EmployeeForm onSubmit={handleSubmit(onSubmit)} error={isError ? (error as Error).message : ""}>
@@ -43,7 +36,7 @@ const EmployeeFormContainer = () => {
 
       <LoadingButton
         type="submit"
-        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center"
         isLoading={isLoading}>
         Submit
       </LoadingButton>

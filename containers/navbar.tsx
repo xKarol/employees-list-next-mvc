58352271b7d4d8474fdd2e9f16568dd0b87b6553
xKarol <karolpiskorz8@gmail.com>
2022-10-08 12:25:48 +0200

    Add routes constants

--- containers/navbar.tsx
+++ containers/navbar.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
 import React from "react";
 
+import { ROUTE_ADD_EMPLOYEE, ROUTE_HOME } from "../constants";
+
 const NavbarContainer = () => {
   return (
     <nav className="px-5 py-2 text-sm mb-5">
       <ul className="flex justify-between">
         <li>
-          <Link href="/">
+          <Link href={ROUTE_HOME}>
             <a>Employees List</a>
           </Link>
         </li>
         <li>
-          <Link href="/add">
+          <Link href={ROUTE_ADD_EMPLOYEE}>
             <a>Add Employee</a>
           </Link>
         </li>
